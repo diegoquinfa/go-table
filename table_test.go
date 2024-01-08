1dcfb41e4b594da78a442b07b376aefc838ff67c
Diego Quintana <dquintana@utb.edu.co>
2024-01-08 02:00:04 -0500

    test update

--- table_test.go
+++ table_test.go
@@ -4,14 +4,14 @@ import "testing"
 
 func TestFunction(t *testing.T) {
 	table, _ := NewTable(
-		NewRow("Primer nombre", "Primer apellido", "Segundo apellido", "Estado Civil", "Descripción"),
-		NewRow("Maria", "Gomez", "Perez", "Divorciada", "Mujer trabajadora y dedicada, le gusta la lectura y el arte."),
-		NewRow("Juan", "Meléndez", "Puello", "Casado", "Es un hombre amigable con una personalidad extrovertida y una risa contagiosa."),
-		NewRow("Carlos", "Salcedo", "", "Soltero", "Ingeniero de Sistemas apasionado por el desarrollo web."),
-		NewRow("Ana", "Rodríguez", "Fernández", "Casada", "Madre de dos hijos, disfruta cocinar y hacer ejercicio."),
-		NewRow("Luis", "Vargas", "Castro", "Soltero", "Estudiante de medicina con una fuerte inclinación hacia la investigación biomédica."),
-		NewRow("Elena", "Martínez", "Santos", "Viuda", "Ama de casa jubilada, disfruta de la jardinería y la lectura."),
-		NewRow("Roberto", "Ramírez", "González", "Comprometido", "Viajero empedernido, ha visitado más de 20 países."),
+		NewRow("Primer nombre", "Primer apellido", "Segundo apellido", "Descripción"),
+		NewRow("Maria", "Gomez", "Perez", "Mujer trabajadora y dedicada, le gusta la lectura y el arte."),
+		NewRow("Juan", "Meléndez", "Puello", "Es un hombre amigable con una personalidad extrovertida y una risa contagiosa."),
+		NewRow("Carlos", "Salcedo", "", "Ingeniero de Sistemas apasionado por el desarrollo web."),
+		NewRow("Ana", "Rodríguez", "Fernández", "Madre de dos hijos, disfruta cocinar y hacer ejercicio."),
+		NewRow("Luis", "Vargas", "Castro", "Estudiante de medicina con una fuerte inclinación hacia la investigación biomédica."),
+		NewRow("Elena", "Martínez", "Santos", "Ama de casa jubilada, disfruta de la jardinería y la lectura."),
+		NewRow("Roberto", "Ramírez", "González", "Viajero empedernido, ha visitado más de 20 países."),
 	)
 
 	table.Print()
